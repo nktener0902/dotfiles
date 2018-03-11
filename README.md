7f8ce66c357609869a4b95d098f14b5a7930f035
Yu Nakata <y-nakata@outlook.com>
2018-03-11 18:39:31 +0900

    Add shell script to get git-prompt.sh and deploy it on /home/ubuntu.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Showing git branch name in bash console
+
+chmod 755 get_git-prompt.sh
+sh get_git-prompt.sh
--- get_git-prompt.sh
+++ get_git-prompt.sh
@@ -1,1 +1,3 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+mv git-prompt.sh .git-prompt.sh
+mv .git-prompt.sh $HOME

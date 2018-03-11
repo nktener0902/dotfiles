641e8224e0530b19d4820ca46fec2af1585220c7
Yu Nakata <y-nakata@outlook.com>
2018-03-11 18:52:34 +0900

    Deleted .bash_profile, and modified get_git-prompt.sh that appends script to show git branch name in bash console.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ```
 sh get_git-prompt.sh
+source $HOME/.bash_profile
 ```
 
 Please check above script before executing it.
--- get_git-prompt.sh
+++ get_git-prompt.sh
@@ -1,10 +1,9 @@
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 mv git-prompt.sh .git-prompt.sh
 mv .git-prompt.sh $HOME
-cat <<EOF
-EOF >> ~/.bash_profile
+cat <<EOF >> $HOME/.bash_profile
 if type __git_ps1 > /dev/null 2>&1 ; then
-  PROMPT_COMMAND="__git_ps1 '\h:\W \u' '\\\$ '; $PROMPT_COMMAND"
+  PROMPT_COMMAND="__git_ps1 '\u@\h:\W' '\\\$ '; $PROMPT_COMMAND"
   GIT_PS1_SHOWDIRTYSTATE=true
   GIT_PS1_SHOWSTASHSTATE=true
   GIT_PS1_SHOWUNTRACKEDFILES=true

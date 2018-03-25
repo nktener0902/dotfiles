002571e691199164e0c78b3e8c8baa31a15c43e1
Yu Nakata <y-nakata@outlook.com>
2018-03-25 17:06:21 +0900

    Get proxy info from HTTP_PROXY

--- init.el
+++ init.el
@@ -84,6 +84,23 @@
     (dolist (pkg not-installed)
         (package-install pkg))))
 
+;; Proxy setting
+(cond ((getenv "HTTP_PROXY")
+       (let* ((url_ (url-generic-parse-url (getenv "HTTP_PROXY")))
+	      (auth_ (if (and (url-user url_) (url-password url_) )
+		      (base64-encode-string
+		       (format "%s:%s" (url-user url_) (url-password url_)))
+		      nil))
+	      (host_ (format "%s:%s" (url-host url_) (url-portspec url_))))
+
+	(defvar url-proxy-services
+	 (list (cons "no_proxy"  "^\\(localhost\\|10.*\\)")
+	  (cons "http" host_)))
+	(if auth_
+	 (defvar url-http-proxy-basic-auth-storage
+	  (list (list host_ (cons "Proxy" auth_)))))
+       )))
+
 
 ;;
 ;; YaTeX

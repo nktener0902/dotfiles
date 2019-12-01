625c5885a028ba81bcd2f0b6ad73176e7d27cc5a
Yu Nakata <y-nakata@outlook.com>
2019-12-01 21:50:05 +0900

    Modify view of git branch and status

--- git/get_git-prompt.sh
+++ git/get_git-prompt.sh
@@ -7,9 +7,9 @@ mv .git-prompt.sh $HOME
 cat <<EOF >> $HOME/.bash_profile
 source $HOME/.git-completion.bash
 source $HOME/.git-prompt.sh
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWUPSTREAM=1
-GIT_PS1_SHOWUNTRACKEDFILES=
-GIT_PS1_SHOWSTASHSTATE=1
-export PS1='\[\033[1;32m\]\u\[\033[00m\]:\[\033[1;34m\]\w\[\033[1;31m\]\[\033[00m\] $ '
+export PS1='\[\033[32m\]\u@\h \[\033[33m\]\w\[\033[36m\]$(__git_ps1)\[\033[0m\]\n$ '
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
 EOF
